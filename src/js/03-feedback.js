033e1eb9f7da323903872e073980d747e917d094
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-05-12 23:18:27 +0300

    финиш

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
 import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const messageArea = document.querySelector('textarea');
+const emailArea = document.querySelector('input');
+const formData = {};
+
+onTextAreaInput();
+
+
+form.addEventListener('submit', onFormSubmit);
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+ 
+    formData[event.target.name] = event.target.value;
+
+  
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));      
+  }
+      , 500)
+);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+     const formElements = event.currentTarget.elements;
+    //  console.log(formElements);
+
+     const mail = formElements.email.value;
+     const text = formElements.message.value;
+   
+   
+    localStorage.removeItem(STORAGE_KEY);
+   
+
+     console.log(`"email" - ${mail}, "message" - ${text}`);
+    form.reset();
+   
+}
+
+
+
+function onTextAreaInput(event) {
+    const savedText = localStorage.getItem(STORAGE_KEY);
+    const parsedText = JSON.parse(savedText);
+  if (parsedText) {
+
+      emailArea.value = parsedText.email;
+      messageArea.value = parsedText.message;
+  }
+}
+
+
+
+
+
